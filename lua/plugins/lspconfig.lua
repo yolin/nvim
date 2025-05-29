6731460ea5f428e788ff6410847b9abb032c297d
yolin <yolin.tsai@senao.com>
2025-05-29 09:27:29 +0800

    lsp: semanticTokens.dynamicRegistration = false

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,8 +3,16 @@ return {
   opts = {
     autoformat = false,
     setup = {
-      clangd = function(_,opts)
-      opts.cmd = {"clangd", "--header-insertion=never"}
+      clangd = function(_, opts)
+        opts.cmd = { "clangd", "--header-insertion=never" }
+        opts.capabilities = vim.tbl_deep_extend("force", opts.capabilities or {}, {
+          textDocument = {
+            semanticTokens = {
+              dynamicRegistration = false,
+            },
+          },
+        })
+        return true
       end,
     },
   },
