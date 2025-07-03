241bf70dba51dc33164e18c5aaaaa369b95241e1
yolin <yolin.tsai@senao.com>
2025-07-03 16:17:05 +0800

    listchars

--- init.lua
+++ init.lua
@@ -3,4 +3,3 @@
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,12 @@
 -- Add any additional options here
 vim.g.autoformat = false
 vim.g.mapleader = ","
+vim.opt.listchars = {
+  tab = "▸ ",     -- tab 顯示為 ▸ 後接一個空格
+  space = "·",    -- space 顯示為 ·
+  trail = "~",    -- 行尾空白顯示為 ~
+  extends = "»",  -- 捲動超出畫面時右側顯示為 »
+  precedes = "«"  -- 捲動超出畫面時左側顯示為 «
+}
+
 
